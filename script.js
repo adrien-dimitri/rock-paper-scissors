b7f29b7e09e8f8770142d7363d8de7d1b2965bb4
adrien-dimitri <amaugueret@gmail.com>
2021-11-20 14:33:08 +0100

    added score count

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*
-function userInput() {
-    const validInput = ["rock", "paper", "scissors"]
-    let user; 
-    while (!(validInput.includes(user))) {
-        console.log('(Enter "0" to exit)');
-        user = prompt("What's your choice").toLowerCase();
-        if (user === "0"){
-            return exit();
-        }
-    }  
-    return user;
-}
-*/
-
 function computerPlay() {
     let randomNumber = Math.floor((Math.random()*3)+1);
     switch (randomNumber) {
@@ -25,38 +10,50 @@ function computerPlay() {
     }
 }
 
+let playerScore = 0;
+let comScore = 0;
+
 function playRound(playerSelection, computerselection) {
-    if (playerSelection == computerselection) {
-        return `It's a tie! You both chose ${playerSelection}`;
+    while (playerScore  < 5 && comScore < 5) {
+        if (playerSelection == computerselection) {
+            return `It's a tie! You both chose ${playerSelection}. Score is ${playerScore}:${comScore}`;
+        }
+        else {
+            if (playerSelection == "rock") {
+                if (computerselection == "scissors") {
+                    playerScore += 1;
+                    return `You Win! Rock beats Scissors. Score is ${playerScore}:${comScore}`;
+                } else{
+                    comScore += 1;
+                    return `You lose! Paper beats Rock. Score is ${playerScore}:${comScore}`;
+                }
+            }
+            if (playerSelection == "paper") {
+                if (computerselection == "rock") {
+                    playerScore += 1;
+                    return `You Win! Paper beats Rock. Score is ${playerScore}:${comScore}`;
+                } else{
+                    comScore += 1;
+                    return `You lose! Scissors beats Paper. Score is ${playerScore}:${comScore}`;
+                }
+            }
+            if (playerSelection == "scissors") {
+                if (computerselection == "paper") {
+                    playerScore += 1;
+                    return `You Win! Scissors beats Paper. Score is ${playerScore}:${comScore}`;
+                } else{
+                    comScore += 1;
+                    return `You lose! Rock beats Scissors. Score is ${playerScore}:${comScore}`;
+                }
+            }
+        }
     }
-    else {
-        if (playerSelection == "rock") {
-            if (computerselection == "scissors") {
-                return "You Win! Rock beats Scissors";
-            } else{
-                return "You lose! Paper beats Rock";
-            }
-        }
-        if (playerSelection == "paper") {
-            if (computerselection == "rock") {
-                return "You Win! Paper beats Rock";
-            } else{
-                return "You lose! Scissors beats Paper";
-            }
-        }
-        if (playerSelection == "scissors") {
-            if (computerselection == "paper") {
-                return "You Win! Scissors beats Paper";
-            } else{
-                return "You lose! Rock beats Scissors";
-            }
-        }
-    }
+    if (playerScore == 5) return `You won the match ${playerScore}:${comScore}!`
+    else return `You lost the match ${playerScore}:${comScore}. Better luck next time :)`
 }
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
-
     button.addEventListener("click" , () => {
         const container = document.querySelector(".container");
         const result = document.createElement("div");
